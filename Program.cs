818670d9b1e3c84ebb29f8e1ca79c078c776f45a
Rizvan Gereykhanov <43379153+Gereykhanov@users.noreply.github.com>
2022-07-14 15:23:45 +0300

    Added files

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Extensions.Polling;
+
+
+var botClient = new TelegramBotClient("5279723562:AAGTpy0zQBrCReJv3Io8u-kaQmObsji0coA");
+
+var meBotState = await botClient.GetMeAsync();
+
+var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+
+
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = Array.Empty<UpdateType>() // receive all update types
+};
+
+botClient.StartReceiving(
+    HandleUpdateAsync,
+    HandlePollingErrorAsync,
+    receiverOptions,
+    cts.Token
+);
+
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message) return;
+
+    // Only process text messages
+    if (message.Text is not { } messageText) return;
+
+    var chatId = message.Chat.Id;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    // Echo received message text
+    Message sentMessage = await botClient.SendTextMessageAsync
+    (
+        chatId: chatId,
+        text: "You said:\n" + messageText,
+        cancellationToken: cancellationToken
+    );
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
